d8e897120b683ad3dfa6d59b3dbff84234822194
Matthew Homan <matthew.homan@cerner.net>
2019-10-31 10:34:50 -0400

    Change font to light, thin was hard to read.

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './fonts/Gotham-Thin.otf'
+import './fonts/Gotham-Light.otf'
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
