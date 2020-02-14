a69cc8c3ac4e8d63412255b8c7afa5a152ede2c9
Matthew Homan <matthew.homan@cerner.net>
2020-02-14 15:11:07 -0500

    convert contact to hooks

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -1,33 +1,21 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import LinedTitle from '../common/LinedTitle';
 import Base from '../common/Base';
 
-class Contact extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        };
+const Contact = () => {
+    const [formValues, setFormValues] = useState({});
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+    function handleChange(event) {
+        formValues[event.target.name] = event.target.value;
+        setFormValues(formValues);
     }
 
-    componentDidMount() {
-
-    }
-
-    handleChange(event) {
-        this.setState({
-            [event.target.name]: event.target.value
-        });
-    }
-
-    handleSubmit(event) {
+    function handleSubmit(event) {
         event.preventDefault();
 
-        let subject = escape(this.state.Subject).trim();
-        let name = escape(this.state.Name).trim();
-        let message = escape(this.state.Message).trim();
+        let subject = escape(formValues.Subject).trim();
+        let name = escape(formValues.Name).trim();
+        let message = escape(formValues.Message).trim();
         
         let link = "mailto:matthew.h.homan@gmail.com"
                     + "?subject=" + subject + " - " + name + " - (From PS Contact)"
@@ -36,34 +24,32 @@ class Contact extends Component {
         window.location.href = link;
     }
     
-    render() {
-        return (
-            <Base header="Matthew&nbsp;Homan" 
-                childComponent={
-                    <div>
-                        <LinedTitle title="Contact" margBottom></LinedTitle>
-                        <form onSubmit={this.handleSubmit} 
-                                style={{width:"100%",maxWidth:"500px",marginLeft:"auto",marginRight:"auto"}}>
-                            <label>Name*</label>
-                            <input type="text" name="Name" placeHolder="Name" required onChange={this.handleChange}/>
-                            <br/>
-                            <br/>
-                            <label>Subject*</label>
-                            <input type="text" name="Subject" placeHolder="Subject" required onChange={this.handleChange}/>
-                            <br/>
-                            <br/>
-                            <label>Message*</label>
-                            <textarea type="text" name="Message" placeHolder="Message" required onChange={this.handleChange}
-                                    style={{height:"200px", width:"100%"}}/>
-                            <br/>
-                            <br/>
-                            <input type="submit" value="Submit" />
-                        </form>
-                    </div>
-                }>
-            </Base>
-        )
-    }
+    return (
+        <Base header="Matthew&nbsp;Homan" 
+            childComponent={
+                <div>
+                    <LinedTitle title="Contact" margBottom></LinedTitle>
+                    <form onSubmit={handleSubmit} 
+                            style={{width:"100%",maxWidth:"500px",marginLeft:"auto",marginRight:"auto"}}>
+                        <label>Name*</label>
+                        <input type="text" name="Name" placeHolder="Name" required onChange={handleChange}/>
+                        <br/>
+                        <br/>
+                        <label>Subject*</label>
+                        <input type="text" name="Subject" placeHolder="Subject" required onChange={handleChange}/>
+                        <br/>
+                        <br/>
+                        <label>Message*</label>
+                        <textarea type="text" name="Message" placeHolder="Message" required onChange={handleChange}
+                                style={{height:"200px", width:"100%"}}/>
+                        <br/>
+                        <br/>
+                        <input type="submit" value="Submit" />
+                    </form>
+                </div>
+            }>
+        </Base>
+    )
 }
 
 export default Contact;
